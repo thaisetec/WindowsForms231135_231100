20df05219619b2317270fe0e73fdd8f97632546c
Usuario <Usuario@CPD02-M20>
2024-09-24 12:17:42 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsForms231135_231100/Banco.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace WindowsForms231135_231100
+{
+    public class Banco
+    {
+        public static MySqlConnection Conexao;
+
+        public static MySqlCommand Comando;
+
+        public static MySqlDataAdapter Adaptador;
+
+        public static DataTable DatTabela;
+
+
+        public static void AbrirConexao()
+        {
+            try
+            {
+                Conexao = new MySqlConnection("server=localhost;port=3307;uid=root;pwd=etecjau");
+                Conexao.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        public static void FecharConexao()
+        {
+            try
+            {
+                Conexao.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        public static void CriarBanco()
+        {
+            try
+            {
+                AbrirConexao();
+                Comando = new MySqlCommand("CREATE DATABASE IF NOT EXISTS vendas; USE vendas", Conexao);
+                Comando.ExecuteNonQuery();
+
+                FecharConexao();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    
+
+public static void CriarBanco();
+{
+
+}
+            
+       
+

d1578a56310e7c56e04bfc2e4a783cb47ba0272f
thaisetec <thaisnegraoetec@gmail.com>
2024-10-01 12:21:39 -0300

    .

--- WindowsForms231135_231100/Banco.cs
+++ WindowsForms231135_231100/Banco.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 namespace WindowsForms231135_231100
@@ -49,6 +50,12 @@ namespace WindowsForms231135_231100
                 AbrirConexao();
                 Comando = new MySqlCommand("CREATE DATABASE IF NOT EXISTS vendas; USE vendas", Conexao);
                 Comando.ExecuteNonQuery();
+                Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS Cidades; USE vendas" +
+                    "(id integer auto_increment primary key," +
+                    "nome char(40)," +
+                    "uf char(02))", Conexao);
+
+                Comando.ExecuteNonQuery();
 
                 FecharConexao();
 
@@ -58,12 +65,14 @@ namespace WindowsForms231135_231100
                 MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+    }
+}
     
 
-public static void CriarBanco();
-{
 
-}
+       
+                
+        
             
        
 
---
+++ WindowsForms231135_231100/Models/Cidade.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using MySql.Data.MySqlclient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WindowsForms231135_231100.Models
+{
+    public class Cidade
+    {
+        public int ID { get; set; }
+        public string nome { get; set; }
+        public string uf { get; set; }
+
+
+
+        public void incluir()
+        {
+            try
+            {
+                Banco.AbrirConexao();
+                Banco.Comando = new MySqlCommand("INSERT INTO cidades (nome, uf) VALUES (@nome, @uf)", Banco.Conexao);
+                Banco.Comando.Parameters.AddWithValue("@nome, nome");
+                Banco.Comando.Parameters.AddWithValue("@uf, uf");
+                Banco.Comando.ExecuteNonQuery();
+                Banco.FecharConexao();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+        public void alterar()
+        {
+            try
+            {
+                Banco.AbrirConexao();
+                Banco.Comando = new MySqlCommand("Update cidades set nome =@nome,uf =@uf where id =@id", Banco.Conexao);
+                Banco.Comando.Parameters.AddWithValue("@nome", nome);
+                Banco.Comando.Parameters.AddWithValue("@uf", uf);
+                Banco.Comando.Parameters.AddWithValue("@id", id);
+
+                Banco.Comando.ExecuteNonQuery();
+                Banco.FecharConexao();
+
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        public void Excluir()
+        {
+            try
+            {
+                Banco.AbrirConexao();
+                Banco.Comando = new MySqlCommand("delete from cidades where id=@id", Banco.Conexao);
+                Banco.Comando.Parameters.AddWithValue("@id", id);
+                Banco.Comando.ExecuteNonQuery();
+                Banco.FecharConexao();
+            }
+            catch (Exception e)
+            {
+
+
+
+
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public DataTable consultar()
+        {
+            try
+            {
+                Banco.AbrirConexao();
+                Banco.Comando = new MySqlCommand("SELECT * FROM cidades where nome like @nome" +
+                    "order by nome", Banco.Conexao);
+                Banco.Comando.parameters.AddWithValue("@nome", nome + "%");
+                Adaptador = new MySqlDataAdapter(Banco.Comando);
+                Banco.Adaptador.Fill(Banco.DatTabela);
+                Banco.FecharConexao();
+                return Banco.DatTabela
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
